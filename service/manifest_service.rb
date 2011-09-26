0952ba10df5e68b4830180cac7e852e9b8285863
Nick <nick@ubuntu.(none)>
2011-09-26 08:31:51 -0400

    Added test service to read manifest file from joseki

--- service/manifest_service.rb
+++ service/manifest_service.rb
@@ -1,14 +1,45 @@
 require File.dirname(__FILE__) + '/util/util.rb'
 
+require 'net/http'
+require 'uri'
+require 'cgi'
+
 module WebServicesKickstart
   class ManifestService < Sinatra::Base
     
     # setting up the environment
     env_index = get_application_setting("-e", "SINATRA_ENV", "development")
     data_dir = get_application_setting("-d", "DATA_DIR", File.dirname(__FILE__) + "/samples/n3/")
+    joseki_host = get_application_setting("-h", "JOSEKI_HOST", "http://localhost:2020")
 
     if (!File.exists? data_dir) || (!File.directory? data_dir)
       raise "Data directory is not configured properly"
+    end
+        
+    get '/manifest/test' do
+      url = URI.parse('http://localhost:2020')
+      res = Net::HTTP.start(url.host, url.port) {|http|
+        http.get('/manifest?query=' + CGI::escape('CONSTRUCT {$a $b $c } WHERE {$a $b $c}'))
+      }
+      
+      graph = RDF::Graph.new()
+      RDF::Reader.for(:rdfxml).new(res.body.to_s) do |reader|
+        reader.each_statement do |statement|
+          graph << statement
+        end
+      end
+      
+      writer = RDF::Writer.for(:json)
+      output = RDF::JSON::Writer.buffer do |writer|
+        graph.each_statement do |statement|
+          writer << statement
+        end
+      end
+
+      # TODO try that with sinatra-jsonp
+      # content_type 'application/json'
+      content_type 'text/plain'
+      output
     end
 
     # image service (with file extension)
